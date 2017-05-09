2e524df92a6d3af42e56774b634a31d950904411
Jeffrey Davis <jeffreypdavis@yahoo.com>
2017-05-09 15:46:08 -0400

    Added Apache License

--- Test_3D_Intersect.cpp
+++ Test_3D_Intersect.cpp
@@ -1,8 +1,20 @@
 /* 
+ * Copyright 2017 Jeffrey Davis
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
  * File:   Test_3D_Intersect.cpp
  * Author: Jeffrey Davis
- * 
- * Created on November 8, 2016, 9:28 AM
  */
 
 #include "Test_3D.h"
